c7217b58d8a9f28bb6fd235d4f45cac5dc25a9fa
Isaac <57078036+MrMiffs@users.noreply.github.com>
2025-02-24 14:53:25 -0700

    Pulled working index.js from server to local

--- jsapp/index.js
+++ jsapp/index.js
@@ -1,82 +1,51 @@
 #!/usr/bin/env node
 
-const http = require('http');
+const http = require('http')
 
-const dancers = [];
-
+const dancers = []
 
 const handleRequest = (req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-  const path = parsedUrl.pathname;
-  const query = parsedUrl.query;
+  const [path, query] = req.url.split('?')
 
-  if (req.method === 'GET' && path === '/api') {
-    // GET endpoint
-    if (query.id) {
-      const dancer = dancers.find(d => d.id === query.id);
-      if (dancer) {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(dancer));
-      } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ error: 'Dancer not found' }));
-      }
+  if (path === '/api') {
+    if (req.method === 'POST') {
+      let body = ''
+      req.on('data', (data) => {
+        body += data
+      })
+      req.on('end', () => {
+        try {
+          const params = Object.fromEntries(body.split('&').map(
+            (param) => param.split('=')
+          ))
+          dancers.push(params)
+          kbye(res)
+        } catch (error) {
+          res.writeHead(400, { "Content-Type": "application/json" })
+          res.end(JSON.stringify({ error: "Invalid request body" }))
+        }
+      })
+    } else if (req.method === 'GET') {
+      kbye(res)
     } else {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(dancers));
-    }
-  } else if (req.method === 'POST' && path === '/api') {
-    // POST endpoint
-    let body = '';
-    req.on('data', (data) => {
-      body += data;
-    });
-    req.on('end', () => {
-      const params = Object.fromEntries(body.split('&').map((param) => param.split('=')));
-      params.id = Date.now().toString(); // Generate a unique ID
-      dancers.push(params);
-      res.writeHead(201, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(params));
-    });
-  } else if (req.method === 'PUT' && path === '/api') {
-    // PUT endpoint
-    let body = '';
-    req.on('data', (data) => {
-      body += data;
-    });
-    req.on('end', () => {
-      const params = Object.fromEntries(body.split('&').map((param) => param.split('=')));
-      const index = dancers.findIndex(d => d.id === params.id);
-      if (index !== -1) {
-        dancers[index] = { ...dancers[index], ...params };
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(dancers[index]));
-      } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ error: 'Dancer not found' }));
-      }
-    });
-  } else if (req.method === 'DELETE' && path === '/api') {
-    // DELETE endpoint
-    if (query.id) {
-      const index = dancers.findIndex(d => d.id === query.id);
-      if (index !== -1) {
-        dancers.splice(index, 1);
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Dancer deleted' }));
-      } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ error: 'Dancer not found' }));
-      }
-    } else {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ error: 'ID parameter is required' }));
+      res.writeHead(405, { "Content-Type": "application/json" })
+      res.end(JSON.stringify({ error: "Method not allowed" }))
     }
   } else {
-    res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ error: 'Endpoint not found' }));
+    res.writeHead(404, { "Content-Type": "application/json" })
+    res.end(JSON.stringify({ error: "Endpoint not found" }))
   }
 }
 
+const kbye = (res) => {
+  res.writeHead(200, {
+    "Content-Type": "application/json"
+  })
+  res.write(JSON.stringify(dancers))
+  res.end()
+}
+
 const server = http.createServer(handleRequest)
-server.listen(3000)
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
